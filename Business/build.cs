b701130885013a5469b8e0cf531e9d3e932007fe
delpijedi <luke.blyth@gmail.com>
2021-12-27 23:38:15 +0000

    changed to use commandlineparser and refactoring

--- Business/build.cs
+++ Business/build.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CommandLine;
 
 namespace ModelCreator.CLI.Business
 {
     [Verb("build", HelpText = "create a new public class based on the commma seperated string")]
-    public class build : ICommand
+    public class BuilderOptions 
     {
         [Option('o',"outFileName", Required = false, HelpText = "creates the file based on your choosing otherwise will default to folder name")]
         public string? FileName { get; set; }
-        [Option('c', "commaSeperatedString", Required = true, HelpText = "the string that will create the model based on comma seperated strings")]
+        [Option('s', "commaSeperatedString", Required = true, HelpText = "the string that will create the model based on comma seperated strings")]
         public string? CommaSeperatedString { get; set; }
-        public void Execute()
-        {
-            Console.WriteLine($"Executing the following: {FileName}, with the following seperated values: {CommaSeperatedString}");
-        }
+       
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,36 +1,18 @@
-﻿class ModelCreatorCLI
+﻿using CommandLine;
+using ModelCreator.CLI.Business;
+
+class program
 {
     public static void Main(string[] args) 
-    { 
-        if (args.Length == 0)
-        {
-            System.Console.WriteLine("Please enter some arguments to continue.");
-            Environment.Exit(0);
-        }
-        var command = args[0];
-        if (!commandMap.ContainsKey(command))
-	{
-		Console.WriteLine("Invalid command");
-	}
-
-	commandMap[command](args.Skip(1).ToArray());
-
+    {
+       var result = Parser.Default.ParseArguments<BuilderOptions>(args)
+      .WithParsed((BuilderOptions opts) => {buildClass(opts); });
     }
 
-    private static readonly Dictionary<string, Action<string[]>> commandMap = new Dictionary<string, Action<string[]>>(StringComparer.InvariantCultureIgnoreCase)
+    static int buildClass(BuilderOptions opts)
     {
-        [nameof(FileName)] = FileName,
-        [nameof(CommaSeperatedString)] = CommaSeperatedString
-    };
-
-
-    static void FileName(string[] args)
-    {
-        System.Console.WriteLine("Executing Filename");
-    }
-
-    static void CommaSeperatedString(string[] args)
-    {
-        System.Console.WriteLine("Comma Seperated String");
+        Console.WriteLine($"File Name: {opts.FileName}");
+        Console.WriteLine($"comma seperated string {opts.CommaSeperatedString}");
+        return 0;
     }
 }
